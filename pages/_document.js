5df09cbbffb2327dfd477d9a7a85595907337e33
Oussama <eljidoussama155@gmail.com>
2023-09-03 15:27:59 +0200

    modifie adscence

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script data-ad-client="YOUR_ADSCENSE8CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
